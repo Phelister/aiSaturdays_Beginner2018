2180c3d0cbbd341bf453feeb67e21394f047098d
Phelister <42007416+Phelister@users.noreply.github.com>
2018-08-01 20:14:37 +0000

    Update task2.py

--- task2.py
+++ task2.py
@@ -4,3 +4,10 @@ words = [word.rstrip('\n') for word in open('words.txt')]
 randomPhrase = " ".join([words[random.randrange(0, len(words))] for i in range(4)])
 
 randomPhrase
+
+reversePhrase= randomPhrase[::-1]
+
+print(randomPhrase) #prints the random chosen phrase so that one can compare to the reversed phrase
+
+print(reversePhrase) #prints the reversed phrase
+
